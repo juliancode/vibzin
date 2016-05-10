435fd9750b4fd752e86fc553f938898deee4fb85
juliancode <julianmorganpearce@gmail.com>
2016-05-10 01:50:28 +0100

    fixed db for heroku

--- js/db.js
+++ js/db.js
@@ -1,5 +1,6 @@
-var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/playlist');
+var mongoose = require("mongoose"),
+	MONGODBURI = "mongodb://heroku_nt5vtnbx:47egirav8vfqp82ac3hg5i8qt9@ds053794.mlab.com:53794/heroku_nt5vtnbx";
+mongoose.connect(MONGODBURI);
 
 var Video = mongoose.model('Video', {
     id: String,
